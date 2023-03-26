1324776f4b0bb4d845670269de3aa3be04728a54
sebastian <sebastianswms@gmail.com>
2023-03-26 14:14:30 -0400

    Comments.

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
 
+const tickTime = 1000; // in milliseconds.
+
 // initialize ideology
 var ideologyAmount = 0;
 var ideologyRate = 1;
-
-// increment ideology
-
-window.ideologyRate = function ideologyRate() {
-    let amount = 1;
-    amount += zines;
-    amount += bricks*2;
-    return amount;
-}
-
-function ideologyTick() {
-    updateIdeology(ideologyRate())
-}
-
-function updateIdeology(n) {
-    ideologyAmount += n;
-    $("#ideologyAmountElement").html(ideologyAmount);
-}
 
 // initialize generators
 var zines = 0;
@@ -31,6 +15,36 @@ var activistsPrice = 10;
 var riots = 0;
 var riotsPrice = 10;
 
+/*
+    Input: The player's current generators and upgrades.
+    Output: The amount to increase the player's ideology each tick.
+*/
+window.ideologyRate = function ideologyRate() {
+    let amount = 1;
+    amount += zines;
+    amount += bricks*2;
+    return amount;
+}
+
+/*
+    Input: None.
+    Output: The player's ideology incrementing by an appropriate amount for a single tick.
+*/
+function ideologyTick() {
+    updateIdeology(ideologyRate())
+}
+
+/*
+    Setter function for ideology. All changes to ideology should be done through this function.
+
+    Input: An amount to increase the player's ideology.
+    Output: The player's ideology changing by an appropriate amount, and HTML being updated.
+*/
+function updateIdeology(n) {
+    ideologyAmount += n;
+    $("#ideologyAmountElement").html(ideologyAmount);
+}
+
 // increment generators
 function zinesTick() {
     zines += activists;
@@ -41,7 +55,10 @@ function bricksTick() {
     $("#bricksAmountElement").html(bricks);
 }
 
-// buy generators
+/*
+    Input: None.
+    Output: If the player has the appropriate amount of ideology, they buy a zine.
+*/
 function zinesBuy() {
     if(zinesPrice <= ideologyAmount) {
         zines++;
@@ -49,6 +66,11 @@ function zinesBuy() {
         $("#zinesAmountElement").html(zines);
     }
 }
+
+/*
+    Input: None.
+    Output: If the player has the appropriate amount of ideology, they buy a brick.
+*/
 function bricksBuy() {
     if(bricksPrice <= ideologyAmount) {
         bricks++;
@@ -57,16 +79,22 @@ function bricksBuy() {
     }
 }
 
-// button listeners
+/*
+    Input: All DOM elements of the page being loaded.
+    Output: Appropriate listeners being added to all buttons on the page.
+*/
 $(document).ready(function() {
     $("#buyZineButton").click(function(){zinesBuy()});
     $("#buyBrickButton").click(function(){bricksBuy()});
 });
 
-// increment time
+/*
+    Input: How long a tick is.
+    Output: All tick functions being called during each tick.
+*/
 window.setInterval(function(){
     ideologyTick();
     zinesTick();
     bricksTick();
-},1000)
+},tickTime)
 
