0acb2427f03c8020da942d9c25836ddacefbc5af
sebastian <sebastianswms@gmail.com>
2023-03-26 11:58:40 -0400

    Increment a counter for amount of Ideology.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 
+let currencyAmount = 0;
+
+function currencyTick() {
+    currencyAmount++;
+    $("#currencyAmountElement").html(currencyAmount);
+}
+
+window.setInterval(function(){
+    ideologyTick();
+},1000)
